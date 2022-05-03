282accef8c7de3362bef144bbd909af6312662e2
pillai-amal <mail.vortextv@gmail.com>
2022-05-03 22:04:09 +0530

    first file

---
+++ appsrc/appfuncs/appfuncs/simplefuncs/SimpleMathFuncs.java
@@ -0,0 +1,11 @@
+package appfuncs.simplefuncs;
+
+public class SimpleMathFuncs {
+    //to determine if a is a facor og b 
+    public static boolean isFactor(inta a, intb b) {
+        if (b%a == 0) return true;
+        return false;
+    }
+}
+
+
